15465a28fc13604eb90f5b8f581a12899bfe6537
Deepesh Sontakke <32305096+Deepesh-1@users.noreply.github.com>
2026-09-23 00:49:17 +0530

    Implement search and sorting features in cheatsheet.js
    Added sorting functionality and toggle for user names.

--- React-JS/cheatsheet.js
+++ React-JS/cheatsheet.js
@@ -1,2 +1,16 @@
 //Search functionality
 const filteredData = data.filter((item)=>(item.name.toLowerCase().includes(query.toLowerCase())))
+//Sorting functionality
+ [...users].sort((a,b)=> a.name.localeCompare(b.name))
+ [...users].sort((a,b)=> b.name.localeCompare(a.name))
+//Sorting toggle
+const [sortType, setSortType] = useState("ascending")
+  const toggleSort = () => {
+    const toggleSortType = [...users].sort((a, b) =>
+      sortType === 'ascending'
+        ? b.name.localeCompare(a.name)
+        : a.name.localeCompare(b.name)
+    );
+    setUsers(toggleSortType);
+    setSortType((prev) => (prev === 'ascending' ? 'descending' : 'ascending'));
+  };
