6993d62efdcca2acadd359c47fb96de0692ccda2
Deepesh Sontakke <32305096+Deepesh-1@users.noreply.github.com>
2026-09-22 23:43:02 +0530

    Update search.js

--- React-JS/search.js
+++ React-JS/search.js
@@ -10,14 +10,28 @@ export default function App() {
       id: 2,
       name: "Bob Martinez",
       department: "Design",
-    }
+    },
   ];
-  const [query, setQuery] = useState("")
-  const filteredData = EMPLOYEES.filter((item)=>(item.name.toLowerCase().includes(query.toLowerCase())))
+  const [query, setQuery] = useState("");
+  const filteredData = EMPLOYEES.filter((item) =>
+    item.name.toLowerCase().includes(query.toLowerCase()),
+  );
   return (
     <div className="App">
-     <input placeholder="Search products" type="text" onChange={(e)=>{setQuery(e.target.value)}}/>
-     <ul>{ filteredData.map((item)=> (<li style={{listStyle:"none"}} value={query} key={item.id}>{item.name}</li> ))}</ul>
+      <input
+        placeholder="Search products"
+        type="text"
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      />
+      <ul>
+        {filteredData.map((item) => (
+          <li style={{ listStyle: "none" }} value={query} key={item.id}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
