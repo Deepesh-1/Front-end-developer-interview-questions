cd5247465e0df98f1e9502fa63664ef8e61e133d
Deepesh Sontakke <32305096+Deepesh-1@users.noreply.github.com>
2026-09-26 17:10:28 +0530

    Update useMemo-vs-useCallback-vs-ReactMemo.js

--- React-JS/useMemo-vs-useCallback-vs-ReactMemo.js
+++ React-JS/useMemo-vs-useCallback-vs-ReactMemo.js
@@ -1,3 +1,27 @@
-useMemo
+// useMemo:caches the result of a calculation.
 
-useMemo caches the result of a calculation.
+const filteredUsers = useMemo(() => {
+  return users.filter(user => user.active);
+}, [users]);
+
+// React recalculates the value only when users changes.
+// useMemo → "Remember this VALUE"
+
+
+// useCallback
+const handleClick = useCallback(() => {
+  console.log('Clicked');
+}, []);
+// The function reference remains the same between renders unless its dependencies change.
+// useCallback → "Remember this FUNCTION"
+
+//React.memo: memoizes a component.
+
+const User = React.memo(function User({ name }) {
+  console.log('User rendered');
+
+  return <div>{name}</div>;
+});
+
+// React.memo → "Remember this COMPONENT" 
+// prevent User from rerendering when props are mot chaged
